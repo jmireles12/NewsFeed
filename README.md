4b8b3501b6f8167f443a1a0c54b160e5abec9ff4
Juan Mireles <jmireles12@gmail.com>
2021-02-22 11:26:36 -0600

    fixed spelling errors

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NewsFeed
 
-Welcome to NewsFeed where you can add Tabs and display news containing the name of the Tabs. I created this website to make searching news a little bit easier. Instead of having different tabs in your browser where you could get different articles or blogs, NewsFeed will only show you news and you can go back and forward from tha Tabs that you created from the drop down.
+Welcome to NewsFeed where you can add Tabs
+and display news containing the name of the Tabs. I created this website to make searching for news a little bit easier. Instead of having different tabs in your browser where you could get different articles or blogs, NewsFeed will only show you news and you can go back and forward from the Tabs that you created from the drop-down. In 'My News' just press the edit button and you'll be able to create a tab with the name of your choosing.
 
 ## Live App
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -18,12 +18,11 @@ export default class Home extends Component {
                 <h2 className='welcome__title'>Welcome</h2>
                 <p className='welcome__message'>Welcome to NewsFeed where you can add Tabs
                 and display news containing the name of the Tabs.
-                I created this website to make searching news a
-                little bit easier. Instead of having different tabs
-                in your browser where you could get different articles
-                or blogs, NewsFeed will only show you news and you
-                can go back and forward from the Tabs that you created
-                from the drop down. In 'My News' just press the edit button and you'll be
+                I created this website to make searching for news a
+                little bit easier. Instead of having different tabs in your 
+                browser where you could get different articles or blogs, NewsFeed 
+                will only show you news and you can go back and forward from the Tabs that you 
+                created from the drop-down. In 'My News' just press the edit button and you'll be 
                 able to create a tab with the name of your choosing.</p>
                 <Link to='/mynews'><button>Go to News</button></Link>
             </header>
